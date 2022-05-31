df2b1680c966b6e80874f9301c48b2fd9c292d23
AJmal <96195451+Brahm03@users.noreply.github.com>
2022-05-31 14:26:05 +0500

    filter page and Doctor info page have been added

--- lib/pages/home/view/pages/doctor/pages/view/filterview.dart
+++ lib/pages/home/view/pages/doctor/pages/view/filterview.dart
@@ -1,5 +1,7 @@
 import 'package:emed/core/constants/font/fontStyles.dart';
 import 'package:emed/core/init/navigator/NavigationService.dart';
+import 'package:emed/extension/sizeExtension.dart';
+import 'package:emed/pages/home/cubit/home_cubit.dart';
 import 'package:emed/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,22 +12,69 @@ class FilterView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        children: [
-          AppBarWidget(
-            leading: InkWell(
-                onTap: () => NavigationService.instance.pop,
-                child: const Text(
-                  'canel',
-                  style: FontStyles.headline3sblue,
-                )),
-            center: const Text(
-              'Filter',
-              style: FontStyles.headline3s,
-            ),
+      child: SingleChildScrollView(
+        child: SizedBox(
+          child: Column(
+            children: [
+              AppBarWidget(
+                leading: InkWell(
+                    onTap: () => NavigationService.instance.pop,
+                    child: const Text(
+                      'canel',
+                      style: FontStyles.headline3sblue,
+                    )),
+                center: const Text(
+                  'Filter',
+                  style: FontStyles.headline3s,
+                ),
+              ),
+               ExpansionTile(
+                title: const Text(
+                  'Positions',
+                  style: FontStyles.headline3s,
+                ),
+                children: [
+                  SizedBox(
+                    height: context.h * 0.5,
+                    child: ListView.builder(
+                        itemCount: HomeCubit.datainfo[0].length,
+                        itemBuilder: (__, _) {
+                          return ListTile(
+                            leading: Checkbox(
+                              onChanged: (v) {},
+                              value: true,
+                            ),
+                            title: Text(HomeCubit.datainfo[0][_]['expert']),
+                          );
+                        }),
+                  )
+                ],
+              ),
+              ExpansionTile(
+                title: const Text(
+                  'Regions',
+                  style: FontStyles.headline3s,
+                ),
+                children: [
+                  SizedBox(
+                    height: context.h * 0.5,
+                    child: ListView.builder(
+                        itemCount: HomeCubit.datainfo[0].length,
+                        itemBuilder: (__, _) {
+                          return ListTile(
+                            leading: Checkbox(
+                              onChanged: (v) {},
+                              value: true,
+                            ),
+                            title: Text(HomeCubit.datainfo[0][_]['region']),
+                          );
+                        }),
+                  )
+                ],
+              )
+            ],
           ),
-          ExpansionTile(title: Text('data'))
-        ],
+        ),
       ),
     ));
   }
